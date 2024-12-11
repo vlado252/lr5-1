2010be6b98e2b603aebd7091737ee2ad04ceed64
vlado252 <vladgremlin1337@gmail.com>
2024-12-11 09:29:15 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-str=input("Введи строку:")
-print("Длина строки = ",len(str))
-glasn=0
-soglasn=0
-a = "аоуыеёиэяю"
-b = "!,.?1234567890' '#№&+=/*-_@^%\""
-for i in str:
-        if i in a:
-             glasn+=1
-        elif i in b:
-             continue
-        else:
-              soglasn+=1
-print("Гласных букв = ",glasn)
-print("Согласных букв = ",soglasn)
+#Напишите программу, которая запрашивает у пользователя строку и выводит ее длину,
+# количество гласных и солганных символов. (Строки вводятся на русском)
+n=str(input("Введите строку:"))
+print(n)
+length = len(n)
+print(f"Длина строки: {length}")
+vowels = "аеёиоуыэюя"
+consonants = "бвгджзйклмнпрстфхцчшщ"
+vowel_count = 0
+consonant_count = 0
+for char in n.lower():
+    if char in vowels:
+        vowel_count += 1
+    elif char in consonants:
+        consonant_count += 1
+
+print(f"Количество гласных: {vowel_count}")
+print(f"Количество согласных: {consonant_count}")
